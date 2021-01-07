796c54a55b44eef491fd6a58469ca1cc41b940ec
Kushagra Bainsla <kushagrabainsla@gmail.com>
2021-01-07 18:45:17 +0530

    added

--- src/App.js
+++ src/App.js
@@ -47,10 +47,7 @@ function SignIn() {
     auth.signInWithPopup(provider);
   }
   return (
-    <>
     <button onClick={signInWithGoogle}>Sign in with Google</button>
-    <p>Do not violate the community guidelines or you will be banned for life!</p>
-    </>
   )
 }
 
